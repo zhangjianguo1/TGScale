4149eb602130f995405d105b89d554288dff282b
张建国 <376564390@qq.com>
2026-04-22 22:52:05 +0800

    【修改】部署问题修改；

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // output: "standalone",
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
