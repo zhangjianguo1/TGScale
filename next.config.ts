b3029f1ad07179bf9426796a054f6e8e5edceb90
张建国 <376564390@qq.com>
2026-04-23 20:59:30 +0800

    【修改】部署问题修改 0423-2059；

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 const nextConfig: NextConfig = {
   // output: "standalone",
   images: {
@@ -15,5 +17,9 @@ const nextConfig: NextConfig = {
     ];
   },
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
